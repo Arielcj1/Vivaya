974ad4d0ed2a25353f588d91e3525bff15cdbcc8
manuelRojasv <manurojasvera@outlook.com>
2021-05-27 21:53:35 -0400

    Merge branch 'master' of https://github.com/Arielcj1/Vivaya

---
+++ cypress/integration/Tests/Admin/a32_SeminarPage.spec.js
@@ -0,0 +1,86 @@
+/// <reference types = "cypress"/>
+
+import {Commons} from "../../../Commons/common"
+import {SeminarPage} from "../../../page-objects-admin/SeminarPage"
+import { HomePage } from "../../../page-objects/Home";
+import {Dashboard} from "../../../page-objects/Dashboard";
+import {SeminarCreationPage} from "../../../page-objects/SeminarCreationPage"
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+})
+
+describe('Verify Seminar Page admin', ()=>{
+    const commons = new Commons()
+    const homePage = new HomePage()
+    const dashboard = new Dashboard()
+    const seminars = new SeminarCreationPage()
+    const seminarPage = new SeminarPage()
+
+    beforeEach(()=>{
+        commons.open_Admin_Site()
+        commons.set_Admin_Credentials()
+    })
+
+    it('Test elements are present in Seminar page',()=>{
+        seminarPage.select_Seminar_Option()
+        seminarPage.select_Seminar_List()
+        //verify elements
+        cy.get('h1').should('contain', 'Seminars')
+        cy.get(':nth-child(1) > .form-group > .control-label').should('have.text','Guide Name')
+        cy.get('#seminarsearch-guidename').should('be.visible')
+        cy.get(':nth-child(2) > .form-group > .control-label').should('have.text','Seminar Name')
+        cy.get('#seminarsearch-name').should('be.visible')
+        cy.get(':nth-child(3) > .form-group > .control-label').should('have.text','From Date')
+        cy.get('#seminarsearch-fromdate').should('be.visible')
+        cy.get(':nth-child(4) > .form-group > .control-label').should('have.text','To Date')
+        cy.get('#seminarsearch-todate').should('be.visible')
+        cy.get(':nth-child(5) > .form-group > .control-label').should('have.text','Public')
+        cy.get('#seminarsearch-public').should('be.visible')
+
+        cy.get('.box-footer > .btn').should('be.visible').and('have.text', 'Search')
+        cy.get(':nth-child(3) > .box-title').should('be.visible').and('have.text', 'List')
+        cy.get('[style="width:50px;"] > .desc').should('be.visible').and('have.text', 'Id')
+        cy.get(':nth-child(2) > .desc').should('be.visible').and('have.text', 'Name')
+        cy.get('thead > tr > :nth-child(3)').should('be.visible').and('have.text', 'Guide Name')
+        cy.get('thead > tr > :nth-child(4)').should('be.visible').and('have.text', 'Start Date')
+        cy.get('thead > tr > :nth-child(5)').should('be.visible').and('have.text', 'End Date')
+        cy.get('thead > tr > :nth-child(6)').should('be.visible').and('have.text', 'Published')
+
+    })
+
+    it('Create a Seminar in Website', ()=>{
+        commons.open_Web_Site()
+        homePage.select_Login()
+        commons.set_Guide_Credentials_One()
+        homePage.submit_Credentials()
+        dashboard.go_to_Dashboard_From_Menu()
+        dashboard.select_Add_Seminar()
+        seminars.type_Seminar_Name("Seminar Manu")
+        seminars.add_Description("This is a Seminar for Automation Proj")
+        seminars.add_Custom_Start_Date(8)
+        seminars.add_Custom_End_Date(15)
+        seminars.select_Main_Offering('Yoga')
+        seminars.add_Seminar_Price(150)
+        seminars.select_Allow_Independent_Events()
+        seminars.select_Publish_Button()
+
+        cy.get('#w0-success-0').should('contain', 'Seminar has been created.')
+        cy.get('h4 > a').should('contain', 'Seminar Manu')
+
+    })  
+    
+    it('Test elements are present in Seminar page',()=>{
+        seminarPage.select_Seminar_Option()
+        seminarPage.select_Seminar_List()
+        seminarPage.search_A_Seminar('Seminar Manu')
+        seminarPage.click_Search_Button()
+        cy.get('tbody > tr > :nth-child(2)').should('be.visible').and('contain.text', 'Seminar Manu')
+        cy.wait(3000)
+        //delete the seminar and veirfy semianr is deleted
+        seminarPage.select_TrashIcon()
+        seminarPage.search_A_Seminar('Seminar Manu')
+        seminarPage.click_Search_Button()
+        cy.get('.empty').should('be.visible').and('contain.text', 'No results found.')
+    })    
+})    
---
+++ cypress/page-objects-admin/SeminarPage.js
@@ -0,0 +1,25 @@
+///<reference types="cypress"/>
+export class SeminarPage {
+
+    select_Seminar_Option(){
+        cy.get(':nth-child(7) > [href="#"] > :nth-child(2)').click()
+    }
+
+    select_Seminar_List(){
+        cy.get('.menu-open > .treeview-menu > :nth-child(1) > a').click()
+    }
+
+    search_A_Seminar(value){
+        cy.get('#seminarsearch-name').type(value)
+    }
+
+    click_Search_Button(){
+        cy.get('.box-footer > .btn').click()
+    }
+    //After perfom a search for specific Seminar, the user select the first Trash icon
+    //in order to delete the seminar founded    
+    select_TrashIcon(){
+        cy.xpath("//span[@class='glyphicon glyphicon-trash']").click()
+    }
+    
+}
