ce98723d6c82710482bb12d9807a7fe5a0f00b39
manuelRojasv <horcosio@gmail.com>
2021-07-26 22:40:18 -0400

    Add test to verify Bottom bar

--- cypress/page-objects-mobile/Home.js
+++ cypress/page-objects-mobile/Home.js
@@ -37,4 +37,42 @@ export class Home {
         cy.get('.become-a-guide > a').click()
     }
 
+    go_To_About_BottomBar(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(1) > a').click()
+    }
+
+    go_To_FAQ(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(2) > a').click()
+    }
+
+    go_To_BecomeaGuide_From_Bottom(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(3) > a').click()
+    }
+
+    go_To_Workshops_From_Bottom(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(4) > a').click()
+    }
+
+    go_To_Seminars_From_Bottom(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(5) > a').click()
+    }
+
+    go_To_Blog_From_Bottom(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(6) > a').click()
+    }
+
+    go_To_Contact_From_Bottom(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(7) > a').click()
+    }
+
+    go_To_PPolicy_From_Bottom(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(8) > a').click()
+    }
+
+    go_To_TermsConditions_From_Bottom(){
+        cy.get('.menu-mobile-footer-part > :nth-child(1) > #footernav > :nth-child(9) > a').click()
+    }
+
+
+
 }
