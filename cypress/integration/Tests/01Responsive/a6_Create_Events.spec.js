1ef8e4b31e0a65c5fbdc76c8a9d78402e2978b01
manuelRojasv <horcosio@gmail.com>
2021-07-27 21:29:14 -0400

    Add test to create events and seminars

---
+++ cypress/integration/Tests/01Responsive/a6_Create_Events.spec.js
@@ -0,0 +1,82 @@
+/// <reference types="cypress"/>
+
+import {Commons} from "../../../Commons/Common"
+import { HomePage } from "../../../page-objects/Home";
+import {Dashboard} from "../../../page-objects/Dashboard";
+import {EventCreationPage} from "../../../page-objects/EventCreationPage"
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+
+
+describe('Elements verification in responsive mode for Events creation', ()=>{
+
+    const commons = new Commons()
+    const homePage = new HomePage()
+    const dashboard = new Dashboard()
+    const eventCreationPage = new EventCreationPage()
+
+    context('Iphone 12Pro resolution', () => {
+        
+        beforeEach(() => {
+        
+          cy.viewport(390, 844)
+          commons.open_Web_Site()
+          commons.login_As_Guide_Mobile()
+        })
+
+        it('Verify Elements in Guide Dashboard after create a class', ()=>{
+            cy.get(':nth-child(3) > .btn').click()
+            cy.get('[href="/events/class/create"]').click()
+
+            eventCreationPage.add_EventName('Class1')
+            cy.wait(2000)
+            eventCreationPage.add_Description('This is a Test Class')
+            eventCreationPage.add_Custom_Number_Of_Days(2)
+            eventCreationPage.custom_Start_Time(5)
+            eventCreationPage.press_Add()
+            cy.get('#w0-success-0').should('contain', 'Events have been created.')
+            dashboard.cancel_Created_Event_class()
+            cy.get('#w0-success-0').should('contain', 'Event has been canceled.')
+
+        })
+
+        it('Verify Elements in Guide Dashboard after create a Dashboard', ()=>{
+            cy.get(':nth-child(3) > .btn').click()
+            cy.get('[href="/events/workshop/create"]').click()
+            eventCreationPage.add_EventName('Workshop1')
+            cy.wait(2000)
+            eventCreationPage.add_Description('This is a Test Workshop')
+            eventCreationPage.add_Custom_Number_Of_Days(2)
+            eventCreationPage.add_Price('40')
+            eventCreationPage.press_Add()
+            cy.get('#w0-success-0').should('contain', 'Events have been created.')
+            dashboard.cancel_Created_Event_Workshop()
+            cy.get('#w0-success-0').should('contain', 'Event has been canceled.')
+
+        })    
+
+        it('Verify Elements in Guide Dashboard after create a 1-1', ()=>{
+            cy.get(':nth-child(3) > .btn').click()
+            cy.get('[href="/events/1-on-1/create"]').click()
+            eventCreationPage.add_EventName('One-One')
+                cy.wait(2000)
+                eventCreationPage.add_Description('This is a Test for One on One')
+                eventCreationPage.add_Custom_Number_Of_Days(3)
+                eventCreationPage.custom_Start_Time(5)
+                cy.xpath('/html/body/div[2]/div[2]/div/div/div/form/div[10]/div/div/div[1]/label/input').click()
+                cy.xpath('/html/body/div[2]/div[2]/div/div/div/form/div[10]/div/div/div[2]/label/input').click()
+                eventCreationPage.press_Add()
+                cy.get('#w0-success-0').should('contain', 'Events have been created.')
+                dashboard.cancel_Created_Event_one_on_one()
+                cy.get('#w0-success-0').should('contain', 'Event has been canceled.')
+
+
+        })    
+
+
+        
+    })
+    
+})    
---
+++ cypress/integration/Tests/01Responsive/a7_Create_Seminars.spec.js
@@ -0,0 +1,58 @@
+/// <reference types="cypress"/>
+
+import {Commons} from "../../../Commons/Common"
+import { HomePage } from "../../../page-objects/Home";
+import {Dashboard} from "../../../page-objects/Dashboard";
+import {EventCreationPage} from "../../../page-objects/EventCreationPage"
+import {SeminarCreationPage} from "../../../page-objects/SeminarCreationPage"
+
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+
+
+describe('Elements verification in responsive mode for Events creation', ()=>{
+
+    const commons = new Commons()
+    const homePage = new HomePage()
+    const dashboard = new Dashboard()
+    const eventCreationPage = new EventCreationPage()
+    const seminars = new SeminarCreationPage()
+
+    context('Iphone 12Pro resolution', () => {
+        
+        beforeEach(() => {
+        
+          cy.viewport(390, 844)
+          commons.open_Web_Site()
+          commons.login_As_Guide_Mobile()
+        })
+
+        it('Verify Elements in Guide Dashboard after create a class', ()=>{
+            cy.get(':nth-child(4) > .btn').click()
+            seminars.type_Seminar_Name("Seminar Automation")
+            seminars.add_Description("This is a Seminar for Automation Proj")
+            seminars.add_Custom_Start_Date(8)
+            //seminars.add_Custom_End_Date(15)
+            cy.get('#seminar-price').click()
+                 var moment = require('moment');
+                 const endDate = moment().add(15, 'days').format('MMM-DD-YYYY')
+                 const typeDate = cy.get('#seminar-enddate').type(endDate)
+                 cy.get('.field-seminar-price > .help-block').click()
+            seminars.select_Main_Offering('Yoga')
+            seminars.add_Seminar_Price(150)
+            seminars.select_Allow_Independent_Events()
+            seminars.select_Publish_Button()
+    
+            cy.get('#w0-success-0').should('contain', 'Seminar has been created.')
+            cy.get('h4 > a').should('contain', 'Seminar Automation')
+
+            seminars.select_Cancel_Seminar()
+            cy.get('#w0-success-0').should('contain', 'Seminar has been cancelled.')
+
+        })
+        
+    })
+    
+})    
