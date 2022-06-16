65222a7db30fc65dded2fcdf6d9c6588e6348c0f
VladimirVargas-5 <vladimir.vargas.a@gmail.com>
2022-06-16 10:25:55 -0400

    greencheff

--- cypress/integration/Tests/Guide and Seeker Actions/a20_greechef_register.spec.js
+++ cypress/integration/Tests/Guide and Seeker Actions/a20_greechef_register.spec.js
@@ -2,8 +2,8 @@
 
 import {Commons} from "../../../Commons/common"
 import {SeekerCreation} from "../../../page-objects/SeekerCreation"
-import {ReferralPage} from "../../../page-objects-admin/ReferralPage"
 import {SeekerPage} from "../../../page-objects-admin/SeekerPage"
+import {HomePage} from "../../../page-objects/Home"
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
@@ -12,14 +12,15 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('Admin Referral Page', ()=>{
     const commons = new Commons()
     const seekerCreation = new SeekerCreation()
-    const referralPage = new ReferralPage()
-    const seekerpage = new SeekerPage()
+    const seekerpage = new SeekerPage()    
+    const homePage = new HomePage()
 
-      it('Go to a GreenChef Link', ()=>{
+      it('Go to a GreenChef Link and create a new seeker', ()=>{
         cy.visit('https://stage.vivayalive.com/signup/seeker/form?greenchef=true')//referral link of a test user 
+        cy.get('.title-subscription > :nth-child(2) > .badge').should('include.text','Risk free 30 day trial')
         seekerCreation.type_First_Name('Green')
         seekerCreation.type_Last_Name('Chef')
-        seekerCreation.type_Seeker_Email('GreenChef@automation.com')
+        seekerCreation.type_Seeker_Email('automation@greenchef.com')
         //seekerCreation.select_Time_Zone('(UTC-04:00) Georgetown, La Paz, Manaus, San Juan')
         seekerCreation.type_Seeker_Password('password')
         seekerCreation.marking_Checkbox()
@@ -28,9 +29,17 @@ describe('Admin Referral Page', ()=>{
         seekerCreation.type_Card_ExpDate('0225')
         seekerCreation.type_Security_Code('123')
         seekerCreation.type_ZipCode('1234')
-        
         cy.wait(2000)
         cy.get('.seeker-registration-content > h2').should('contain', 'Thank You')
+
+    })
+
+    it('Review that seeker has 30 days trial', ()=>{
+        commons.open_Web_Site()
+        homePage.select_Login()
+        commons.set_Generic_Seeker('automation@greenchef.com', 'password')
+        seekerCreation.seeker_dashboard()
+        cy.get('.col-md-3 > .dashboard-box > div > .count').should('contain','30')
     })
 
     it('Seeker elimination from admin',()=>{
@@ -38,7 +47,7 @@ describe('Admin Referral Page', ()=>{
         commons.set_Admin_Credentials()
         seekerpage.select_Seeker_Option()
         seekerpage.select_Seeker_List()
-        seekerpage.type_Seeker_Email('GreenChef@automation.com')
+        seekerpage.type_Seeker_Email('automation@greenchef.com')
         seekerpage.select_Seeker_options('5') //num 5 for elimination 
         cy.get('#w3-success').should('contain', 'Seeker removed completely successful')
     })
